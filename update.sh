2e0fca5e935f13a1f8a7a0df24211cf60483efc4
nogogame <songzy23@m.fudan.edu.cn>
2024-06-14 15:01:18 +0800

    描述更改的提交信息

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git remote add origin https://github.com/nogogame/PQ-TLS-TEST.git
-git push -u origin master
+# git remote add origin https://github.com/nogogame/PQ-TLS-TEST.git
+# git push -u origin master
 git add .
 git commit -m "描述更改的提交信息"
 git push origin master
