b23d0e8f6d3f4b34a273b10740a71718ff833f47
root <root@DESKTOP-CQG64UC>
2024-06-16 15:04:35 +0800

    Commit at Sun Jun 16 15:04:35 CST 2024

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo git add .
-sudo git commit -m "Commit at $(date)"
-sudo git push origin master
-#sudo git remote remove origin
-#sudo git remote add origin https://github.com/nogogame/pq-tls-test.git
-#sudo git remote -v
-#sudo git push -u origin master
